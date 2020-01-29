f112440d4add1ec4d5f04149df4398189c100bf0
Padmaraj Bhat <padmarajbhat@gmail.com>
2020-01-29 10:48:08 +0530

    reconstructing lost modules and routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { XyzComponent } from './xyz/xyz.component';
+import { ReportsComponent } from './reports/reports.component';
+import { CareerlevelfraudtrendComponent } from './reports/charts/careerlevelfraudtrend/careerlevelfraudtrend.component';
+import { RecruitedbyComponent } from './reports/charts/recruitedby/recruitedby.component';
+import { LmnComponent } from './lmn/lmn.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"", component:LmnComponent},
+  { path: 'xyz', component: XyzComponent },
+  {
+    path: 'reports', component: ReportsComponent,
+    children: [
+      { path: "careerlevel", component: CareerlevelfraudtrendComponent },
+      { path: "recruitedby", component: RecruitedbyComponent },
+    ]
+  },
+  ]
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { AbcdComponent } from './abcd/abcd.component';
+import { LmnComponent } from './lmn/lmn.component';
+import { ReportsComponent } from './reports/reports.component';
+import { CareerlevelfraudtrendComponent } from './reports/charts/careerlevelfraudtrend/careerlevelfraudtrend.component';
+import { RecruitedbyComponent } from './reports/charts/recruitedby/recruitedby.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    XyzComponent,
+    AbcdComponent,
+    LmnComponent,
+    ReportsComponent,
+    CareerlevelfraudtrendComponent,
+    RecruitedbyComponent,
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
