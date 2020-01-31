35c5e0f122e6f3506ac7fbd2d683baf4b7b67bf2
Padmaraj Bhat <padmarajbhat@gmail.com>
2020-01-31 20:29:19 +0530

    failed attempts for responsive screens

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ChangeDetectorRef } from '@angular/core';
+import { Router } from '@angular/router';
 declare var google: any;
 @Component({
   selector: 'app-reports',
@@ -10,15 +11,13 @@ export class ReportsComponent implements OnInit {
   chartsLoaded = false;
   height = window.innerHeight;
   width = window.innerWidth;
-  constructor() { }
+  constructor(private rout : Router) { }
 
   ngOnInit() {
    let url = 'https://www.gstatic.com/charts/loader.js';
     this.loadScript(url).then(() => { console.log("script loaded")}, (err) => { console.log("error while loading script",err)});
    console.log("Loading : ", url, typeof google);
   }
-
-     
 
   private loadScript(styleUrl: string) {
     return new Promise((resolve, reject) => {
@@ -31,6 +30,18 @@ export class ReportsComponent implements OnInit {
       //script.onload = resolve;
       body.appendChild(script);
     });
+  }
+
+  @HostListener('window:orientationchange', ['$event'])
+  onOrientationChange(event) {
+    console.log('orientationChanged');
+    this.height = window.innerHeight;
+    this.width = window.innerWidth;
+    //this.ref.detach();
+    //this.ref.reattach();
+    //this.rout.navigateByUrl('/');
+    console.log("trying to rerender");
+
   }
 
 }
