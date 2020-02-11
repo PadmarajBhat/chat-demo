202e74d0724abcf6ea5dc73e229db309a836bc2a
padmaraj.bhat <padmarajbhat@gmail.com>
2020-02-11 22:18:04 +0530

    responsive attempt#1

--- src/app/reports/charts/relocation-wise-trend/relocation-wise-trend.component.ts
+++ src/app/reports/charts/relocation-wise-trend/relocation-wise-trend.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { DataLoaderService } from '../data-loader.service';
+import { LoadScriptService } from '../../../load-script.service';
+
+declare var google: any;
 
 @Component({
   selector: 'app-relocation-wise-trend',
@@ -8,9 +11,74 @@ import { DataLoaderService } from '../data-loader.service';
 })
 export class RelocationWiseTrendComponent implements OnInit {
 
-  constructor(private dl: DataLoaderService) { }
+  @ViewChild('relocationTrend', { static: false }) relocationTrend: ElementRef;
+  switchOn = true;
+  constructor(
+    private dl: DataLoaderService,
+    private ls: LoadScriptService
+  ) { }
+
 
   ngOnInit() {
   }
 
+  drawChart = () => {
+
+    const data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work', 11],
+      ['Eat', 2.7],
+      ['Commute', 2],
+      ['Watch TV', 2],
+      ['Sleep', 7]
+    ]);
+    var options = {
+      title: 'My Daily Activities',
+      animation: {
+        duration: 10000,
+        easing: 'in'
+      },
+    };
+
+    var chart = new google.visualization.PieChart(this.relocationTrend.nativeElement);
+    chart.draw(data, options);
+  }
+
+  getCareerLevelFraudStyle() {
+    return {
+      'margin': '5%',
+      'background-color': 'white',
+      'display': 'flex',
+      'flex-direction': 'column',
+      'height.px': window.innerHeight * .8,
+    }
+  }
+
+  ngAfterViewInit() {
+    this.ls.loadScript('googleCharts').then(() => {
+      console.log("Google Chart Script got attached to body !!!", google);
+      try {
+
+        google.charts.load('current', { 'packages': ['corechart'] }).then(() => {
+
+          console.log("Google Chart packages loaded at ngAfterViewInit!!!", google);
+
+          google.charts.setOnLoadCallback(this.drawChart());
+        });
+      } catch {
+        console.log("could not load google packages at ngAfterViewInit");
+      }
+
+    });
+    console.log("ngAfterViewInit should be called only once during the component life cycle");
+  }
+
+  @HostListener('window:resize')
+  onOrientationChange() {
+    let myElem = document.getElementById("relocationTrend");
+    myElem.removeChild(myElem.firstChild);
+    google.charts.setOnLoadCallback(this.drawChart());
+
+    //this.relocationTrend.nativeElement.removeChild(this.relocationTrend.nativeElement.firstTrend);
+  }
 }
