903309d6e025e6ac29741dde9d3c03fb7c277d1d
padmaraj.bhat <padmarajbhat@gmail.com>
2020-02-27 06:21:53 +0530

    integrating navigation button highlight

--- src/app/root/reports/report-ui/side-end-draw/side-end-draw.component.ts
+++ src/app/root/reports/report-ui/side-end-draw/side-end-draw.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ViewChild } from '@angular/core';
 import { Platform } from '@angular/cdk/platform';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-side-end-draw',
   templateUrl: './side-end-draw.component.html',
   styleUrls: ['./side-end-draw.component.css']
 })
-export class SideEndDrawComponent implements OnInit {
-
+export class SideEndDrawComponent implements OnInit, AfterViewInit {
+    
+  @ViewChild(CdkVirtualScrollViewport, { static: true }) sidenavContainer: CdkVirtualScrollViewport;
   @Input() dashboard: boolean;
   @Input() dl;
+  lastScrolledIndex: string;
 
   constructor(private platform: Platform) {
     console.log("side :", this.dashboard, this.dl);
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    this.sidenavContainer.scrolledIndexChange.subscribe(
+      (x) => {
+        console.log("ScrolledIndexChange : ", x);
+        this.toggle(x.toString());
+        this.toggle(this.lastScrolledIndex);
+        this.lastScrolledIndex = x.toString();
+      }
+    );
   }
 
   getChartIds() {
@@ -46,5 +60,24 @@ export class SideEndDrawComponent implements OnInit {
     console.log("scrolling");
     let myElem = document.getElementById(id+"_card");
     myElem.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+  }
+
+  toggle(id: string) {
+
+    for (var i = 0; i < this.dl.chartList.getChartList(!this.dashboard).length; i++) {
+      if (this.dl.chartList.getChartList(!this.dashboard)[i].id == "Item " + id) {
+        this.dl.chartList.getChartList(!this.dashboard)[i].enable = !this.dl.chartList.getChartList(!this.dashboard)[i].enable;
+        return;
+      }
+    }
+
+  }
+
+  getActiveStatus(id: string) {
+    for (var i = 0; i < this.dl.chartList.getChartList(!this.dashboard).length; i++) {
+      if (this.dl.chartList.getChartList(!this.dashboard)[i].id == id) {
+        return { 'active': this.dl.chartList.getChartList(!this.dashboard)[i].enable };
+      }
+    }
   }
 }
