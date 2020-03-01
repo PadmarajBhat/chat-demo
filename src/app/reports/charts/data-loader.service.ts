51d7fb2a00f783b7838400a158eec1e6c83d3ac0
padmaraj.bhat <padmarajbhat@gmail.com>
2020-03-01 09:00:58 +0530

    why not inheritence?

--- src/app/reports/charts/data-loader.service.ts
+++ src/app/reports/charts/data-loader.service.ts
@@ -1,21 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { GoogleChartInterface } from './google-chart-interface';
 import { ChartList } from '../../root/reports/ChartList';
-
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataLoaderService {
-  public chartList: ChartList = new ChartList();
+export class DataLoaderService extends ChartList{
   constructor(
+    
     private http: HttpClient,
-  ) { }
+  ) { super();}
 
   getIdData(idName: string) {
-    return this.http.get(this.chartList.getIdUrl(idName));
+    return this.http.get(this.getIdUrl(idName));
   }
 
 }
